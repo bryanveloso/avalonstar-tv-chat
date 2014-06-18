9287bcc0bec40ae96f87fbf146055b2f9d10c8df
Bryan Veloso <bryan@revyver.com>
2014-06-17 21:30:41 -0700

    Handlebars.

--- config/views.js
+++ config/views.js
@@ -26,14 +26,11 @@ module.exports.views = {
 
   // For more options, check out the docs:
   // https://github.com/balderdashy/sails-wiki/blob/0.9/config.views.md#engine
-
-  engine: 'ejs',
-  
-
+  engine: 'handlebars',
 
   // Layouts are simply top-level HTML templates you can use as wrappers
-  // for your server-side views.  If you're using ejs or jade, you can take advantage of
-  // Sails' built-in `layout` support.
+  // for your server-side views.  If you're using ejs or jade, you can take
+  // advantage of Sails' built-in `layout` support.
   //
   // When using a layout, when one of your views is served, it is injected into
   // the `body` partial defined in the layout.  This lets you reuse header
@@ -41,32 +38,15 @@ module.exports.views = {
   //
   // NOTE:  Layout support is only implemented for the `ejs` view engine!
   //        For most other engines, it is not necessary, since they implement
-  //        partials/layouts themselves.  In those cases, this config will be silently
-  //        ignored.
+  //        partials/layouts themselves. In those cases, this config will be
+  //        silently ignored.
   //
   // The `layout` setting may be set to one of the following:
   //
   // If `false`, layouts will be disabled.
-  // Otherwise, if a string is specified, it will be interpreted as the relative path
-  // to your layout file from `views/` folder. (the file extension, ".ejs", should be omitted)
-  //
+  // Otherwise, if a string is specified, it will be interpreted as the relative
+  // path to your layout file from `views/` folder. (the file extension,
+  // ".ejs", should be omitted)
+  layout: false,
 
-  layout: 'layout'
-
-
-
-  // Using Multiple Layouts with EJS
-  //
-  // If you're using the default engine, `ejs`, Sails supports the use of multiple
-  // `layout` files.  To take advantage of this, before rendering a view, override
-  // the `layout` local in your controller by setting `res.locals.layout`.
-  // (this is handy if you parts of your app's UI look completely different from each other)
-  //
-  // e.g. your default might be
-  // layout: 'layouts/public'
-  //
-  // But you might override that in some of your controllers with:
-  // layout: 'layouts/internal'
-
-  
 };
