65c1f7c9ad4fc9462eb084083499d3d1b1a6d06c
Bryan Veloso <bryan@revyver.com>
2014-06-18 17:18:08 -0700

    Model and control for messages (needs IRC adapter).

---
+++ api/controllers/MessagesController.js
@@ -0,0 +1,11 @@
+/**
+ * MessagesController
+ *
+ * @description :: Server-side logic for managing messages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
