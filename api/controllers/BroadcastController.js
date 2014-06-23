a0f673f3639c7c1750701f1846929e5d8569286f
Bryan Veloso <bryan@revyver.com>
2014-06-23 10:12:52 -0700

    Basic model and controller for broadcasts.

---
+++ api/controllers/BroadcastController.js
@@ -0,0 +1,11 @@
+/**
+ * BroadcastController
+ *
+ * @description :: Server-side logic for managing broadcasts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var BroadcastController = {};
+
+module.exports = BroadcastController;
+
---
+++ api/models/Broadcast.js
@@ -0,0 +1,29 @@
+/**
+* Broadcast.js
+*
+* @description :: Represents a single broadcast.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+var Broadcast = {
+  attributes: {
+    number: 'string',
+
+    // Metadata. Dates.
+    created: 'date',
+
+    // Games played.
+    games: 'array',
+
+    // Broadcast statuses (special, charity, etc.)
+    isCharity: 'boolean',
+    isCouple: 'boolean',
+    isMarathon: 'boolean',
+
+    // Status. Much like Twitch's `status`, but can be used for other
+    // purposes, i.e. comments or special guests.
+    status: 'string',
+  }
+};
+
+module.exports = Broadcast;
