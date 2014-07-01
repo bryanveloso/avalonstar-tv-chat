45ee7fe15937073b8dcc036e1d860eca129541b1
Bryan Veloso <bryan@revyver.com>
2014-06-30 22:22:53 -0700

    Use Redis, love Redis.

--- config/connections.js
+++ config/connections.js
@@ -21,6 +21,7 @@
  * For more information on configuration, check out:
  * http://links.sailsjs.org/docs/config/connections
  */
+var rtg = require('url').parse(process.env.REDISTOGO_URL);
 
 module.exports.connections = {
   // Local disk storage for DEVELOPMENT ONLY
@@ -31,47 +32,10 @@ module.exports.connections = {
     adapter: 'sails-disk'
   },
 
-  // MySQL is the world's most popular relational database.
-  // http://en.wikipedia.org/wiki/MySQL
-  //
-  // Run:
-  // npm install sails-mysql
-  //
-  someMysqlServer: {
-    adapter: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
-  },
-
-  // MongoDB is the leading NoSQL database.
-  // http://en.wikipedia.org/wiki/MongoDB
-  //
-  // Run:
-  // npm install sails-mongo
-  //
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
-  },
-
-  // PostgreSQL is another officially supported relational database.
-  // http://en.wikipedia.org/wiki/PostgreSQL
-  //
-  // Run:
-  // npm install sails-postgresql
-  //
-  somePostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
+  redis: {
+    adapter: 'sails-redis',
+    hostname: rtg.hostname,
+    port: rtg.port,
   },
 
   tmi: {
@@ -79,6 +43,7 @@ module.exports.connections = {
     channel: '#avalonstar',
     nick: 'elsydeon',
     host: 'irc.twitch.tv',
+    password: process.env.IRC_PASSWORD
   }
 
   // More adapters:
