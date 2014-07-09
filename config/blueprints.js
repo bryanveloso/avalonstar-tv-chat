af78b110142fbc74978e4c9a5aae361a2cf0eab5
Bryan Veloso <bryan@revyver.com>
2014-07-08 19:18:20 -0700

    Prefix our blueprints with /api.

--- config/blueprints.js
+++ config/blueprints.js
@@ -106,7 +106,7 @@ module.exports.blueprints = {
   // `DELETE /api/v2/foo/:id`
   //
   // By default, no prefix is used.
-  prefix: '',
+  prefix: '/api',
 
 
   // Whether to pluralize controller names in blueprint routes.
