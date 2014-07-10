c09a34fb8422a9c9d2c5ca5d5a69b10897bdb6d6
Bryan Veloso <bryan@revyver.com>
2014-07-09 19:16:59 -0700

    Add routes for broadcasts and viewers.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -5,6 +5,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('broadcasts');
+  this.resource('viewers');
+
   // ...
   this.resource('live', function() {
     this.route('prologue');
