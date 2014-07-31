2c02d77522e9507352f951bda68424e5e34b7364
Bryan Veloso <bryan@revyver.com>
2014-07-30 18:14:33 -0700

    Pusher it real good.

--- app/app.js
+++ app/app.js
@@ -6,7 +6,10 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'avalonstar', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver,
+
+  // Configuration for ember-pusher.
+  PUSHER_OPTS: { key: '207f2c96da3bdb9301f8', connection: {} }
 });
 
 loadInitializers(App, 'avalonstar');
